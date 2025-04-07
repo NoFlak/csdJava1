c30ae464b02ac6aaa91e70e2b68d88a3dc1a133f
Brent <barrows.brent@gmail.com>
2025-04-07 16:05:10 -0700

    This is a combination of 2 commits. This is the 1st commit message: Add CS&141 Assignment 01 files and Week 1 sandbox code This is the 2nd commit message: Add Week 2 Monday Decisions file

---
+++ Week1/sandbox/src/payroll.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class payroll {
+    public static void main(String[] args) {
+        String name;
+        int hours;
+        double payRate;
+        double grossPay;
+
+        Scanner keyboard = new Scanner(System.in);
+        name = keyboard
+
+        system.out.print("What is \'your\' name? ");
+        name = keyboard.nextLine();
+        char x = '"';
+        char y = '\'';
+        String path = "C:\\Users\\User\\Documents\\" + x + name + y + ".txt";
+    }
+}
---
+++ Week1/sandbox/src/sandbox.java
@@ -0,0 +1,14 @@
+public class Sandbox {
+    public static void main(String[] args) {
+        int myVar = 30;
+        System.out.println("Hello, World!");
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("How old are you? ");
+        int age = keyboard.nextInt();
+        Systyem.out.println("You are " + age + " years old.");
+        System.out.print("Next year you will be " + (age + 1) + " years old.");
+        int a1 = 5;
+        int a2 = 2; 
+        int c =  a1 / a2;
+    }
+}   
