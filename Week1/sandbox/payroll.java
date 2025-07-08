22388253ebbf5cf8f93b681d395450926ac9e69a
noflak <barrows.brent@gmail.com>
2025-07-08 10:30:34 -0700

    Moved last quarter into spring2025

--- Week1/sandbox/payroll.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner
-
-public class payroll {
-
-    public static void main(string[ args]);
-
-}
-}
-String s = "";
--- Week1/sandbox/sandbox.java
+++
@@ -1,6 +0,0 @@
-public class sandbox {
-    public static void main(String[] args) {
-        int myVar = 55;
-        System.out.println("Hello world!");
-    }
-}
